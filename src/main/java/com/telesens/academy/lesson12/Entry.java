e0d1e59a7a894276d11bd17c4ad8cc369e542806
Oleg <oleg.kh81@gmail.com>
2018-11-05 19:47:58 +0200

    Comparator

--- src/main/java/com/telesens/academy/lesson12/Entry.java
+++ src/main/java/com/telesens/academy/lesson12/Entry.java
@@ -1,5 +1,59 @@
 package com.telesens.academy.lesson12;
 
-public class Entry {
+public class Entry implements Comparable<Entry> {
+    private String value;
+    private long key;
 
+    public Entry(long key, String value) {
+        this.value = value;
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public long getKey() {
+        return key;
+    }
+
+    public void setKey(long key) {
+        this.key = key;
+    }
+
+    @Override
+    public String toString() {
+        return "Entry{" +
+                "value='" + value + '\'' +
+                ", key=" + key +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Entry)) return false;
+
+        Entry entry = (Entry) o;
+
+
+        if (getKey() != entry.getKey()) return false;
+        return getValue() != null ? getValue().equals(entry.getValue()) : entry.getValue() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getValue() != null ? getValue().hashCode() : 0;
+        result = 31 * result + (int) (getKey() ^ (getKey() >>> 32));
+        return result;
+    }
+
+    @Override
+    public int compareTo(Entry o) {
+        return this.getValue().compareTo(o.getValue());
+    }
 }
--- src/test/java/com/telesens/academy/tests/lesson12/EntryTests.java
+++ src/test/java/com/telesens/academy/tests/lesson12/EntryTests.java
@@ -1,5 +1,7 @@
 package com.telesens.academy.tests.lesson12;
 
+import com.telesens.academy.lesson12.Entry;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class EntryTests {
@@ -7,5 +9,15 @@ public class EntryTests {
     @Test
     public void testSimpleEntry() {
         System.out.println("start 'testSimpleEntry'");
+        long key = 12L;
+        String value = "demoValue";
+
+        Entry entry = new Entry(key, value);
+        Entry entryEqual = new Entry(key, value);
+
+        Assert.assertEquals(key, entry.getKey());
+        Assert.assertEquals(value, entry.getValue());
+
+        Assert.assertEquals(entry, entryEqual);
     }
 }
