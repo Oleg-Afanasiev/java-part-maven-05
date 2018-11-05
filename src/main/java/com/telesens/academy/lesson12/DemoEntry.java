cb5519ffecb2b08dc02ccc1e00cffa44b608b8b7
Oleg <oleg.kh81@gmail.com>
2018-11-05 19:48:14 +0200

    Comparator

---
+++ src/main/java/com/telesens/academy/lesson12/DemoEntry.java
@@ -0,0 +1,23 @@
+package com.telesens.academy.lesson12;
+
+import java.util.Arrays;
+
+public class DemoEntry {
+    public static void main(String[] args) {
+        Entry[] entries = {
+                new Entry(12L, "name1"),
+                new Entry(11L, "name4"),
+                new Entry(10L, "name3")
+        };
+        // Вывести
+        System.out.println(Arrays.toString(entries));
+
+        // Сортировать
+        Arrays.sort(entries);
+        System.out.println(Arrays.toString(entries));
+
+        // Сортировать
+        Arrays.sort(entries, new ByKeyComparator());
+        System.out.println(Arrays.toString(entries));
+    }
+}
