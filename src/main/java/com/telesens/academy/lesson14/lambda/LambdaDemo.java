a28b0e947cdd10851ee0b84b80bc0695c690e419
Oleg <oleg.kh81@gmail.com>
2018-11-09 20:20:03 +0200

    Lambda

---
+++ src/main/java/com/telesens/academy/lesson14/lambda/LambdaDemo.java
@@ -0,0 +1,32 @@
+package com.telesens.academy.lesson14.lambda;
+
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class LambdaDemo {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        String res = doSomethingWithString(str, s->s.toUpperCase());
+        String res2 = doSomethingWithString(str, String::toLowerCase);
+        System.out.println(res);
+        System.out.println(res2);
+        // TODO
+        boolean isInRange = checkNumber(10, null);
+        System.out.println(isInRange);
+//        LambdaDemo lambdaDemo = new LambdaDemo();
+//        String resNext = lambdaDemo.doSomethingWithStringNoStatic(str);
+    }
+
+    private static String doSomethingWithString(String str, Function<String, String> operation) {
+        return operation.apply(str);
+    }
+
+    private static boolean checkNumber(int number, Predicate<Integer> checker) {
+        return checker.test(number);
+
+    }
+
+    private String doSomethingWithStringNoStatic(String str) {
+        return null;
+    }
+}
