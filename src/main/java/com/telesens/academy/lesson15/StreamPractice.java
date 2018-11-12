07193bc6fe21a91529e9cfbd2e75e13bb4616826
Oleg <oleg.kh81@gmail.com>
2018-11-12 20:39:00 +0200

    Stream API practice 2

--- src/main/java/com/telesens/academy/lesson15/StreamPractice.java
+++ src/main/java/com/telesens/academy/lesson15/StreamPractice.java
@@ -7,9 +7,27 @@ import java.util.Arrays;
 
 public class StreamPractice {
     public static void main(String[] args) {
-        EntityDress[] array = CompareDresses.getExpectedDresses();
+        EntityDress[] dresses = CompareDresses.getExpectedDresses();
 
-        Arrays.stream(array)
-                .forEach(System.out::println);
+        // Вывести только с цветом 'Orange'
+        // и с максимальной ценой
+        Arrays.stream(dresses)
+                .filter(s->s.getColor().equals("Orange"))
+                .reduce(StreamPractice::maxByPrice)
+//                .reduce((e1, e2)-> e1.getPrice() > e2.getPrice() ? e1 : e2)
+                .ifPresent(System.out::println);
+
+        // 1) Вычленить только цену
+        // 2) отсортировать по убыванию
+        // 3) Вывести результат
+        // 4) Сохранить в массив Double[] prices
+        Arrays.stream(dresses);
+    }
+
+    private static EntityDress maxByPrice(EntityDress e1, EntityDress e2) {
+        if (e1.getPrice() > e2.getPrice())
+            return e1;
+        else
+            return e2;
     }
 }
