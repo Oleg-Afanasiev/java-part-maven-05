e2619159c2dd0d1b9f8b0724bf58230d0b1146cc
Oleg <oleg.kh81@gmail.com>
2018-11-12 20:09:55 +0200

    Stream API demo

--- src/main/java/com/telesens/academy/lesson14/lambda/LambdaDemo.java
+++ src/main/java/com/telesens/academy/lesson14/lambda/LambdaDemo.java
@@ -8,6 +8,9 @@ public class LambdaDemo {
         String str = "Hello World";
         String res = doSomethingWithString(str, s->s.toUpperCase());
         String res2 = doSomethingWithString(str, String::toLowerCase);
+        Function<String, String> f1 = s->s.toUpperCase();
+        Function<String, String> f2 = String::toLowerCase;
+        String res3 = doSomethingWithString(str, f2);
         System.out.println(res);
         System.out.println(res2);
         // TODO
